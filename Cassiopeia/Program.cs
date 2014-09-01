e64d5dcd8bd05a05465cbeba2766b72205e6c0a9
pqmailer <pqmailer@gmail.com>
2014-09-01 14:57:02 +0200

    update kappa

--- Cassiopeia/Program.cs
+++ Cassiopeia/Program.cs
@@ -14,15 +14,29 @@ namespace Cassio
     internal class Program
     {
         private const string ChampionName = "Cassiopeia";
-        private static Orbwalking.Orbwalker Orbwalker;
-        private static readonly List<Spell> SpellList = new List<Spell>();
-        private static Spell Q;
-        private static Spell W;
-        private static Spell E;
-        private static Spell R;
-        private static Menu Config;
 
-        private static SpellSlot IgniteSlot;
+        private static Orbwalking.Orbwalker _orbwalker;
+
+        private static readonly List<Spell> SpellList = new List<Spell>();
+        private static Spell _q;
+        private static Spell _w;
+        private static Spell _e;
+        private static Spell _r;
+        private static SpellSlot _igniteSlot;
+
+        private static Menu _config;
+
+        private static bool IsPassiveReady
+        {
+            get
+            {
+                return
+                    ObjectManager.Player.Buffs.Any(
+                        buff =>
+                            buff.IsActive && buff.Name.ToLower() == "cassiopeiadeadlycadence" &&
+                            buff.EndTime <= (Game.Time + _e.Delay - 0.2 + Game.Ping / 2000.0));
+            }
+        }
 
         private static void Main(string[] args)
         {
@@ -32,51 +46,53 @@ namespace Cassio
         private static void Game_OnGameLoad(EventArgs args)
         {
             if (ObjectManager.Player.ChampionName != ChampionName)
+            {
                 return;
+            }
 
-            Q = new Spell(SpellSlot.Q, 850);
-            W = new Spell(SpellSlot.W, 850);
-            E = new Spell(SpellSlot.E, 700);
-            R = new Spell(SpellSlot.R, 825);
+            _q = new Spell(SpellSlot.Q, 850);
+            _w = new Spell(SpellSlot.W, 850);
+            _e = new Spell(SpellSlot.E, 700);
+            _r = new Spell(SpellSlot.R, 825);
 
-            IgniteSlot = ObjectManager.Player.GetSpellSlot("SummonerDot");
+            _igniteSlot = ObjectManager.Player.GetSpellSlot("SummonerDot");
 
             const double ultAngle = 80 * Math.PI / 180;
-            const float fUltAngle = (float)ultAngle;
+            const float fUltAngle = (float) ultAngle;
 
-            Q.SetSkillshot(0.60f, 75f, int.MaxValue, false, SkillshotType.SkillshotCircle);
-            W.SetSkillshot(0.50f, 106f, 2500f, false, SkillshotType.SkillshotCircle);
-            R.SetSkillshot(0.30f, fUltAngle, int.MaxValue, false, SkillshotType.SkillshotCone);
+            _q.SetSkillshot(0.60f, 75f, int.MaxValue, false, SkillshotType.SkillshotCircle);
+            _w.SetSkillshot(0.50f, 106f, 2500f, false, SkillshotType.SkillshotCircle);
+            _r.SetSkillshot(0.30f, fUltAngle, int.MaxValue, false, SkillshotType.SkillshotCone);
 
-            SpellList.Add(Q);
-            SpellList.Add(W);
-            SpellList.Add(E);
+            SpellList.Add(_q);
+            SpellList.Add(_w);
+            SpellList.Add(_e);
 
-            Config = new Menu(ChampionName, ChampionName, true);
+            _config = new Menu(ChampionName, ChampionName, true);
 
-            Config.AddSubMenu(new Menu("Orbwalking", "Orbwalking"));
-            Orbwalker = new Orbwalking.Orbwalker(Config.SubMenu("Orbwalking"));
+            _config.AddSubMenu(new Menu("Orbwalking", "Orbwalking"));
+            _orbwalker = new Orbwalking.Orbwalker(_config.SubMenu("Orbwalking"));
 
-            Config.AddSubMenu(new Menu("Combo", "Combo"));
-            Config.SubMenu("Combo")
+            _config.AddSubMenu(new Menu("Combo", "Combo"));
+            _config.SubMenu("Combo")
                 .AddItem(new MenuItem("ComboActive", "Combo!").SetValue(new KeyBind(32, KeyBindType.Press)));
 
-            Config.AddSubMenu(new Menu("Drawings", "Drawings"));
-            Config.SubMenu("Drawings")
-                .AddItem(new MenuItem("QRange", "Q range").SetValue(new Circle(true, Color.FromArgb(255, 255, 255, 255))));
-            Config.SubMenu("Drawings")
+            _config.AddSubMenu(new Menu("Drawings", "Drawings"));
+            _config.SubMenu("Drawings")
+                .AddItem(
+                    new MenuItem("QRange", "Q range").SetValue(new Circle(true, Color.FromArgb(255, 255, 255, 255))));
+            _config.SubMenu("Drawings")
                 .AddItem(
                     new MenuItem("WRange", "W range").SetValue(new Circle(false, Color.FromArgb(255, 255, 255, 255))));
-            Config.SubMenu("Drawings")
+            _config.SubMenu("Drawings")
                 .AddItem(
                     new MenuItem("ERange", "E range").SetValue(new Circle(false, Color.FromArgb(255, 255, 255, 255))));
 
-            Config.AddSubMenu(new Menu("Extra", "Extra"));
-            Config.SubMenu("Extra")
+            _config.AddSubMenu(new Menu("Extra", "Extra"));
+            _config.SubMenu("Extra")
                 .AddItem(new MenuItem("Passive", "Keep Passive Up").SetValue(new KeyBind(114, KeyBindType.Toggle)));
-            Config.SubMenu("Extra")
-                .AddItem(new MenuItem("Minions", "Use Q on minions").SetValue(true));
-            Config.AddToMainMenu();
+            _config.SubMenu("Extra").AddItem(new MenuItem("Minions", "Use Q on minions").SetValue(true));
+            _config.AddToMainMenu();
 
             Drawing.OnDraw += Drawing_OnDraw;
             Game.OnGameUpdate += Game_OnGameUpdate;
@@ -86,46 +102,58 @@ namespace Cassio
 
         private static void Game_OnGameSendPacket(GamePacketEventArgs args)
         {
-            if (args.PacketData[0] != Packet.C2S.Cast.Header) return;
+            if (args.PacketData[0] != Packet.C2S.Cast.Header)
+            {
+                return;
+            }
 
             var decodedPacket = Packet.C2S.Cast.Decoded(args.PacketData);
             if (decodedPacket.SourceNetworkId != ObjectManager.Player.NetworkId || decodedPacket.Slot != SpellSlot.R)
+            {
                 return;
+            }
 
-            if (ObjectManager.Get<Obj_AI_Hero>().Count(hero => R.WillHit(hero, R.GetPrediction(hero).CastPosition)) == 0)
+            if (ObjectManager.Get<Obj_AI_Hero>().Count(hero => _r.WillHit(hero, _r.GetPrediction(hero).CastPosition)) ==
+                0)
+            {
                 args.Process = false;
+            }
         }
 
         private static void Orbwalking_BeforeAttack(Orbwalking.BeforeAttackEventArgs args)
         {
-            args.Process = (!Q.IsReady() && !W.IsReady() && !E.IsReady());
+            args.Process = (!_q.IsReady() && !_w.IsReady() && !_e.IsReady());
         }
 
         private static void Drawing_OnDraw(EventArgs args)
         {
-            if (SpellList == null) return;
+            if (SpellList == null)
+            {
+                return;
+            }
 
             foreach (var spell in SpellList)
             {
-                var menuItem = Config.Item(spell.Slot + "Range").GetValue<Circle>();
+                var menuItem = _config.Item(spell.Slot + "Range").GetValue<Circle>();
 
                 if (menuItem.Active)
+                {
                     Utility.DrawCircle(ObjectManager.Player.Position, spell.Range, menuItem.Color);
+                }
             }
         }
 
         private static void Game_OnGameUpdate(EventArgs args)
         {
-            if (Config.Item("ComboActive").GetValue<KeyBind>().Active)
+            if (_config.Item("ComboActive").GetValue<KeyBind>().Active)
+            {
                 ExecuteCombo();
+            }
 
-            if (Config.Item("Passive").GetValue<KeyBind>().Active)
+            if (_config.Item("Passive").GetValue<KeyBind>().Active)
+            {
                 KeepPassiveUp();
-            
-            //foreach (var buff in ObjectManager.Player.Buffs) {
-            //    if (buff.Name == "cassiopeiadeadlycadence")
-            //        Game.PrintChat("Working");
-            //}
+            }
         }
 
         private static bool IsPoisoned(Obj_AI_Base unit)
@@ -135,79 +163,98 @@ namespace Cassio
                     .Any(buff => buff.EndTime >= (Game.Time + 0.35 + 700 / 1900));
         }
 
-        private static bool IsPassiveReady()
-        {
-            return ObjectManager.Player.Buffs.Where(buff => buff.IsActive && (buff.Name == "cassiopeiadeadlycadence" || buff.Name == "CassiopeiaDeadlyCadence"))
-                .Any(buff => buff.EndTime <= (Game.Time + 0.6 + Game.Ping / 200));
-        }
-
         private static void KeepPassiveUp()
         {
-            if (!Q.IsReady() || !IsPassiveReady())
-                return;
-
-            foreach (var hero in ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsValidTarget()))
+            if (!_q.IsReady() || !IsPassiveReady)
             {
-                if (SharpDX.Vector2.Distance(hero.ServerPosition.To2D(), hero.ServerPosition.To2D()) < Q.Range)
-                    Q.Cast(hero, true, true);
+                return;
             }
 
-            if (Config.Item("Minions").GetValue<bool>())
-            { 
-                foreach (var minion in ObjectManager.Get<Obj_AI_Minion>().Where(minion => minion.IsValidTarget()))
+            foreach (var hero in ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsValidTarget(_q.Range)))
+            {
+                _q.Cast(hero, true, true);
+            }
+
+            if (_config.Item("Minions").GetValue<bool>())
+            {
+                foreach (
+                    var minion in ObjectManager.Get<Obj_AI_Minion>().Where(minion => minion.IsValidTarget(_q.Range)))
                 {
-                    if (SharpDX.Vector2.Distance(minion.ServerPosition.To2D(), minion.ServerPosition.To2D()) < Q.Range)
-                        Q.Cast(minion, true, true);
+                    _q.Cast(minion, true, true);
                 }
             }
 
-            if (Game.CursorPos.To2D().Distance(ObjectManager.Player) < Q.Range) 
-                Q.Cast(Game.CursorPos);
+            if (Game.CursorPos.Distance(ObjectManager.Player.ServerPosition) < _q.Range)
+            {
+                _q.Cast(Game.CursorPos);
+            }
             else
-                Q.Cast(ObjectManager.Player.Position.To2D().Extend(Game.CursorPos.To2D(), Q.Range-Q.Width)); 
-  
+            {
+                _q.Cast(Game.CursorPos.To2D().Extend(ObjectManager.Player.ServerPosition.To2D(), - _q.Range + _q.Width));
+            }
         }
 
         private static void ExecuteCombo()
         {
-            var target = SimpleTs.GetTarget(Q.Range, SimpleTs.DamageType.Magical);
-            if (target == null) return;
-
-            if (Q.IsReady(2000) && E.IsReady(2000) && R.IsReady() && DamageLib.IsKillable(target,
-                new[]
-                {
-                    DamageLib.SpellType.Q, DamageLib.SpellType.W, DamageLib.SpellType.E, DamageLib.SpellType.E,
-                    DamageLib.SpellType.R, DamageLib.SpellType.R, DamageLib.SpellType.IGNITE
-                }))
+            var target = SimpleTs.GetTarget(_q.Range, SimpleTs.DamageType.Magical);
+            if (target == null)
             {
-                if (IgniteSlot != SpellSlot.Unknown &&
-                    ObjectManager.Player.SummonerSpellbook.CanUseSpell(IgniteSlot) == SpellState.Ready)
-                    ObjectManager.Player.SummonerSpellbook.CastSpell(IgniteSlot, target);
+                return;
+            }
 
-                if (ObjectManager.Player.Distance(target) <= R.Range + R.Width)
-                    R.Cast(target, true, true);
+            if (_q.IsReady(2000) && _e.IsReady(2000) && _r.IsReady() &&
+                DamageLib.IsKillable(
+                    target,
+                    new[]
+                    {
+                        DamageLib.SpellType.Q, DamageLib.SpellType.W, DamageLib.SpellType.E, DamageLib.SpellType.E,
+                        DamageLib.SpellType.R, DamageLib.SpellType.R, DamageLib.SpellType.IGNITE
+                    }))
+            {
+                if (_igniteSlot != SpellSlot.Unknown &&
+                    ObjectManager.Player.SummonerSpellbook.CanUseSpell(_igniteSlot) == SpellState.Ready)
+                {
+                    ObjectManager.Player.SummonerSpellbook.CastSpell(_igniteSlot, target);
+                }
 
-                if (ObjectManager.Player.Distance(target) <= W.Range + W.Width)
-                    W.Cast(target, true, true);
+                if (ObjectManager.Player.Distance(target) <= _r.Range + _r.Width)
+                {
+                    _r.Cast(target, true, true);
+                }
 
-                if (ObjectManager.Player.Distance(target) <= Q.Range + Q.Width)
-                    Q.Cast(target, true, true);
+                if (ObjectManager.Player.Distance(target) <= _w.Range + _w.Width)
+                {
+                    _w.Cast(target, true, true);
+                }
 
-                if (ObjectManager.Player.Distance(target) <= E.Range + target.BoundingRadius && IsPoisoned(target) ||
+                if (ObjectManager.Player.Distance(target) <= _q.Range + _q.Width)
+                {
+                    _q.Cast(target, true, true);
+                }
+
+                if (ObjectManager.Player.Distance(target) <= _e.Range + target.BoundingRadius && IsPoisoned(target) ||
                     DamageLib.IsKillable(target, new[] { DamageLib.SpellType.E }))
-                    E.CastOnUnit(target, true);
+                {
+                    _e.CastOnUnit(target, true);
+                }
             }
             else
             {
-                if (W.IsReady() && ObjectManager.Player.Distance(target) <= W.Range + W.Width)
-                    W.Cast(target, true, true);
+                if (_w.IsReady() && ObjectManager.Player.Distance(target) <= _w.Range + _w.Width)
+                {
+                    _w.Cast(target, true, true);
+                }
 
-                if (Q.IsReady() && ObjectManager.Player.Distance(target) <= Q.Range + Q.Width)
-                    Q.Cast(target, true, true);
+                if (_q.IsReady() && ObjectManager.Player.Distance(target) <= _q.Range + _q.Width)
+                {
+                    _q.Cast(target, true, true);
+                }
 
-                if (E.IsReady() && ObjectManager.Player.Distance(target) <= E.Range + target.BoundingRadius &&
+                if (_e.IsReady() && ObjectManager.Player.Distance(target) <= _e.Range + target.BoundingRadius &&
                     IsPoisoned(target) || DamageLib.IsKillable(target, new[] { DamageLib.SpellType.E }))
-                    E.CastOnUnit(target, true);
+                {
+                    _e.CastOnUnit(target, true);
+                }
             }
         }
     }
