8a7a050e4a1ad74f8d329ea92db87dfc1e4ee2ab
Esk0r <kortatu1512@hotmail.com>
2014-10-12 01:02:11 +0200

    Improved sion

--- Sion/Program.cs
+++ Sion/Program.cs
@@ -6,14 +6,20 @@ using System.Text;
 using System.Threading.Tasks;
 using LeagueSharp;
 using LeagueSharp.Common;
+using SharpDX;
 
 namespace Sion
 {
     class Program
     {
         private static Menu Config;
-        private static Obj_AI_Hero Player;
 
+        public static Orbwalking.Orbwalker Orbwalker;
+
+        public static Spell Q;
+        public static Spell E;
+
+        public static Vector2 QCastPos = new Vector2();
         static void Main(string[] args)
         {
             CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
@@ -22,27 +28,85 @@ namespace Sion
         static void Game_OnGameLoad(EventArgs args)
         {
             if (ObjectManager.Player.BaseSkinName != "Sion") return;
-            Player = ObjectManager.Player;
+
+            foreach (var hero in ObjectManager.Get<Obj_AI_Hero>())
+            {
+                foreach (var spell in hero.Spellbook.Spells)
+                {
+                    Console.WriteLine(
+                        spell.SData.Name + " w:" + spell.SData.LineWidth + " s:" + spell.SData.MissileSpeed + " r: " +
+                        spell.SData.CastRange[0]);
+                }
+            }
+
+            //Spells
+            Q = new Spell(SpellSlot.Q, 1050);
+            Q.SetSkillshot(0.6f, 100f, float.MaxValue, false, SkillshotType.SkillshotLine);
+            Q.SetCharged("SionQ", "SionQ", 500, 720, 0.5f);
+
+            E = new Spell(SpellSlot.E, 800);
+            E.SetSkillshot(0.25f, 80f, 1800, false, SkillshotType.SkillshotLine);
+
             //Make the menu
             Config = new Menu("Sion", "Sion", true);
 
-            var R = new Menu("R", "R");
-            R.AddItem(new MenuItem("AntiCamLock", "Avoid locking camera").SetValue(true));
-            R.AddItem(new MenuItem("MoveToMouse", "Move to mouse (Exploit)").SetValue(false));//Disabled by default since its not legit Keepo
-            Config.AddSubMenu(R);
+            //Orbwalker submenu
+            Config.AddSubMenu(new Menu("Orbwalking", "Orbwalking"));
+
+            //Add the target selector to the menu as submenu.
+            var targetSelectorMenu = new Menu("Target Selector", "Target Selector");
+            SimpleTs.AddToMenu(targetSelectorMenu);
+            Config.AddSubMenu(targetSelectorMenu);
+
+            //Load the orbwalker and add it to the menu as submenu.
+            Orbwalker = new Orbwalking.Orbwalker(Config.SubMenu("Orbwalking"));
+
+            //Combo menu:
+            Config.AddSubMenu(new Menu("Combo", "Combo"));
+            Config.SubMenu("Combo").AddItem(new MenuItem("UseQCombo", "Use Q").SetValue(true));
+            Config.SubMenu("Combo").AddItem(new MenuItem("UseWCombo", "Use W").SetValue(true));
+            Config.SubMenu("Combo").AddItem(new MenuItem("UseECombo", "Use E").SetValue(true));
+            Config.SubMenu("Combo").AddItem(new MenuItem("ComboActive", "Combo!").SetValue(new KeyBind(Config.Item("Orbwalk").GetValue<KeyBind>().Key, KeyBindType.Press)));
+
+            
+            Config.AddSubMenu(new Menu("R", "R"));
+            Config.SubMenu("R").AddItem(new MenuItem("AntiCamLock", "Avoid locking camera").SetValue(true));
+            Config.SubMenu("R").AddItem(new MenuItem("MoveToMouse", "Move to mouse (Exploit)").SetValue(false));//Disabled by default since its not legit Keepo
+            
 
             Config.AddToMainMenu();
 
             Game.PrintChat("Sion Loaded!");
             Game.OnGameUpdate += Game_OnGameUpdate;
             Game.OnGameProcessPacket += Game_OnGameProcessPacket;
+            Drawing.OnDraw += Drawing_OnDraw;
+            Obj_AI_Hero.OnProcessSpellCast += ObjAiHeroOnOnProcessSpellCast;
+        }
+
+
+
+        private static void ObjAiHeroOnOnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
+        {
+            if (sender.IsMe && args.SData.Name == "SionQ")
+            {
+                QCastPos = args.End.To2D();
+            }
+        }
+
+        static void Drawing_OnDraw(EventArgs args)
+        {
+            Utility.DrawCircle(ObjectManager.Player.Position, Q.Range, System.Drawing.Color.White);
         }
 
         static void Game_OnGameProcessPacket(GamePacketEventArgs args)
         {
             if (args.PacketData[0] == 0xFE && Config.Item("AntiCamLock").GetValue<bool>())
             {
-                args.Process = false;
+                var p = new GamePacket(args.PacketData);
+                if (p.ReadInteger(1) == ObjectManager.Player.NetworkId && p.Size() > 9)
+                {
+                    args.Process = false;
+                }
             }
         }
 
@@ -59,7 +123,57 @@ namespace Sion
                 return;
             }
 
-            //iMehs job keepo
+            if (Config.Item("ComboActive").GetValue<KeyBind>().Active)
+            {
+                var qTarget = SimpleTs.GetTarget(!Q.IsCharging ? Q.ChargedMaxRange / 2 : Q.ChargedMaxRange, SimpleTs.DamageType.Physical);
+
+                var eTarget = SimpleTs.GetTarget(E.Range, SimpleTs.DamageType.Physical);
+
+                if (qTarget != null && Config.Item("UseQCombo").GetValue<bool>())
+                {
+                    if (Q.IsCharging)
+                    {
+                        var start = ObjectManager.Player.ServerPosition.To2D();
+                        var end = start.Extend(QCastPos, Q.Range);
+                        var direction = (end - start).Normalized();
+                        var normal = direction.Perpendicular();
+
+                        var points = new List<Vector2>();
+                        var hitBox = qTarget.BoundingRadius;
+                        points.Add(start + normal * (Q.Width + hitBox));
+                        points.Add(start - normal * (Q.Width + hitBox));
+                        points.Add(end + Q.ChargedMaxRange * direction - normal * (Q.Width + hitBox));
+                        points.Add(end + Q.ChargedMaxRange * direction + normal * (Q.Width + hitBox));
+
+                        for (int i = 0; i <= points.Count - 1; i++)
+                        {
+                            var A = points[i];
+                            var B = points[i == points.Count - 1 ? 0 : i + 1];
+
+                            if (qTarget.ServerPosition.To2D().Distance(A, B, true, true) < 50 * 50)
+                            {
+                                Packet.C2S.ChargedCast.Encoded(new Packet.C2S.ChargedCast.Struct((SpellSlot)((byte)Q.Slot), Game.CursorPos.X, Game.CursorPos.X, Game.CursorPos.X)).Send();
+                            }
+                        }
+                        return;
+                    }
+                    
+                    if(Q.IsReady())
+                    {
+                        Q.StartCharging(qTarget.ServerPosition);
+                    }
+                }
+
+                if (qTarget != null && Config.Item("UseWCombo").GetValue<bool>())
+                {
+                    ObjectManager.Player.Spellbook.CastSpell(SpellSlot.W, ObjectManager.Player);
+                }
+
+                if (eTarget != null && Config.Item("UseECombo").GetValue<bool>())
+                {
+                    E.Cast(eTarget);
+                }
+            }
         }
     }
 }
