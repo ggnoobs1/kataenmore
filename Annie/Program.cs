94826aa4511bae2aad25c8ef13f882f21b341316
Andre Brandao <andrebrandao@sapo.pt>
2014-11-01 23:43:55 +0000

    Fixed annie

--- Annie/Program.cs
+++ Annie/Program.cs
@@ -206,9 +206,9 @@ namespace Annie
             switch (Orbwalker.ActiveMode)
             {
                 case Orbwalking.OrbwalkingMode.Combo:
-                    Orbwalker.SetAttacks(false);
+                    Orbwalker.SetAttack(false);
                     Combo(target, flashRtarget);
-                    Orbwalker.SetAttacks(true);
+                    Orbwalker.SetAttack(true);
                     break;
                 case Orbwalking.OrbwalkingMode.Mixed:
                     if (Config.Item("suppMode").GetValue<bool>())
