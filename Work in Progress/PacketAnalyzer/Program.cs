e873ef63708997dfc6c7e19f05128256f67e09bf
trees-software <treesbot@gmail.com>
2014-11-24 13:31:52 -0500

    Fix Crash

--- Work in Progress/PacketAnalyzer/Program.cs
+++ Work in Progress/PacketAnalyzer/Program.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Windows.Forms;
 using LeagueSharp;
 using LeagueSharp.Common;
@@ -18,52 +19,58 @@ namespace PacketAnalyzer
         public static List<GamePacket> SendPackets = new List<GamePacket>();
         public static List<GamePacket> RecvPackets = new List<GamePacket>();
         public static List<byte> BlockedSendPackets = new List<byte> { 0xA8, 0x16, 0x14, 0x08, 0x77 };
+        public static Thread T;
 
         [STAThread]
         private static void Main(string[] args)
         {
-            var T = new System.Threading.Thread(Work);
-            T.Start();
-
             Game.OnGameSendPacket += Game_OnGameSendPacket;
             Game.OnGameProcessPacket += Game_OnGameProcessPacket;
+            AppDomain.CurrentDomain.DomainUnload += CurrentDomain_DomainUnload;
+            AppDomain.CurrentDomain.ProcessExit += CurrentDomain_DomainUnload;
+
+            T = new Thread(Work) { IsBackground = true };
+            T.Start();
+        }
+
+        private static void CurrentDomain_DomainUnload(object sender, EventArgs e)
+        {
+            if (T.IsAlive)
+            {
+                T.Abort();
+            }
+
+            Form1.Close();
+            Application.Exit();
         }
 
         private static void Game_OnGameSendPacket(GamePacketEventArgs args)
         {
-            if (Form1.chkSend.Checked && !BlockedSendPackets.Contains(args.PacketData[0]))
+            if (!Form1.chkSend.Checked || BlockedSendPackets.Contains(args.PacketData[0]))
             {
-                var p = new GamePacket(args);
-                Form1.PGridSend.AddTo(p);
-                SendPackets.Add(p);
+                return;
             }
+            var p = new GamePacket(args);
+            Form1.PGridSend.AddTo(p);
+            SendPackets.Add(p);
         }
 
         private static void Game_OnGameProcessPacket(GamePacketEventArgs args)
         {
-            if (Form1.chkRecv.Checked && !BlockedRecvPackets.Contains(args.PacketData[0]))
+            if (!Form1.chkRecv.Checked || BlockedRecvPackets.Contains(args.PacketData[0]))
             {
-                
-                var p = new GamePacket(args);
-                if (p.SearchInteger(ObjectManager.Player.NetworkId) != null)
-                {
-                    Form1.PGridRecv.AddTo(p);
-                    RecvPackets.Add(p);
-                    
-                }
-            
+                return;
             }
-        
+
+            var p = new GamePacket(args);
+
+            if (p.SearchInteger(ObjectManager.Player.NetworkId) != null)
+            {
+                Form1.PGridRecv.AddTo(p);
+                RecvPackets.Add(p);
+            }
         }
 
-        [STAThread]
-        public static void SetClipboardText(ListBox list)
-        {
-            foreach (var item in list.Items)
-            {
-                Clipboard.SetText(item.ToString());
-            }    
-        }
 
         [STAThread]
         public static void Work()
